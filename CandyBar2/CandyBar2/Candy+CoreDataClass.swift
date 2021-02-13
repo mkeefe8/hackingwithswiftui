a4fd7e56e2dc97091a8fbe877d6239ee27688a5e
Mary Keefe <mary_keefe2@hotmail.com>
2021-02-13 11:24:07 -0600

    adding CandyBar2

---
+++ CandyBar2/CandyBar2/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CandyBar2
+//
+//  Created by Mary Keefe on 2/12/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CandyBar2/CandyBar2/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CandyBar2
+//
+//  Created by Mary Keefe on 2/12/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
